b6126b9bb727ae7ba317e3033a44b595e7cd6e26
Javan <psyjavan@gmail.com>
2016-10-20 12:10:43 -0400

    Various nav behavior fixes
    - Fixed side bar nav mobile behavior (nav disappearing on desktop)
    - Added slide animations for hashes (experimental)
    - Reorganized header names (brand and iOS were mislabelled)

--- scripts/main-nav.js
+++ scripts/main-nav.js
@@ -8,14 +8,30 @@ $(document).ready(function() {
 
 	});
 
+	$(function() {
+		$('a[href*="#"]:not([href="#"])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+				if (target.length) {
+					$('html, body').animate({
+						scrollTop: target.offset().top
+					}, 300);
+					return false;
+				}
+			}
+		});
+	});
+
 
 	$(".mobile-nav").click(function() {
 		$('.holder').slideToggle(400);
+		$(".dropdown li a").click(function(){
+			$('.mobile-nav').click();
+			$(this).closest('.dropdown-head').click();
+		});
 	});
 
-	$(".dropdown li a").click(function(){
-		$('.mobile-nav').click();
-		//$(this).closest('.dropdown-head').click();
-	});
+
 
 });
