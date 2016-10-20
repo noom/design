b5230c92e9dab16b3c369d9f08621eebf946e94a
Javan <psyjavan@gmail.com>
2016-10-20 17:14:14 -0400

    Adding/reorganizing sections
    - added sections for web-dashboard and web-marketing
    - merged iOS and Android pages
    - added testing section for hiding irrelevant sections

--- scripts/main-nav.js
+++ scripts/main-nav.js
@@ -8,21 +8,27 @@ $(document).ready(function() {
 
 	});
 
-	$(function() {
-		$('a[href*="#"]:not([href="#"])').click(function() {
-			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-				var target = $(this.hash);
-				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-				if (target.length) {
-					$('html, body').animate({
-						scrollTop: target.offset().top
-					}, 300);
-					return false;
-				}
-			}
-		});
-	});
+	// $(function() {
+	// 	$('a[href*="#"]:not([href="#"])').click(function() {
+	// 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	// 			var target = $(this.hash);
+	// 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	// 			if (target.length) {
+	// 				$('html, body').animate({
+	// 					scrollTop: target.offset().top
+	// 				}, 300);
+	// 				return false;
+	// 			}
+	// 		}
+	// 	});
+	// });
 
+	$('.showSingle').click(function () {
+		$('.targetDiv').hide();
+		$('.showSingle').removeClass('active');
+		$(this).addClass("active")
+		$('#' + $(this).attr('target')).show();
+	});
 
 	$(".mobile-nav").click(function() {
 		$('.holder').slideToggle(400);
